1cc155d55aeb0d1e8bb948d48be136da8c65c88a
LuminoFox <36134481+LuminoFox@users.noreply.github.com>
2022-06-15 01:39:50 +0800

    Add files via upload

---
+++ UVa 11614.py
@@ -0,0 +1,13 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+import math
+x = int(input(""))
+for i in range(x):
+    num=int(input(""))
+    print(math.floor((-1+math.sqrt(1+8*num))/2))
+
